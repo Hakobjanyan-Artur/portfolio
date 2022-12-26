e20df25cfd35c77f32422361cc6dfb77da51c637
arturwebdev <hakobjanyan61@gmail.co>
2022-12-26 15:58:36 +0400

    addFirebase

--- src/components/ Recommendations/Recommendation.js
+++ src/components/ Recommendations/Recommendation.js
@@ -1,14 +1,26 @@
 import './Recommendation.css'
 import img from '../../images/manager.png'
 import recLang from '../../lang/recLang'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectLang } from '../../store/slices/langs/langsSlice'
 import {FaTelegramPlane} from "react-icons/fa"
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import { addNewRec } from '../../store/slices/recommendSlices/recommendslices'
 
 function Recommendations() {
     const lang = useSelector(selectLang)
     const recModal = useRef(null)
+    const [newName, setNewName] = useState('')
+    const [newRelation, setNewRelation] = useState('')
+    const [newRecommend, setNewRecommend] = useState('')
+    const formRef = useRef(null)
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addNewRec({name: newName, relation: newRelation, recommend: newRecommend}))
+        formRef.current.reset()
+    }
 
     return(
         <div 
@@ -74,19 +86,24 @@ function Recommendations() {
                         <h2>
                             {lang === 'eng' ? recLang.eng.recBtn : lang === 'rus' ? recLang.ru.recBtn : recLang.am.recBtn}
                         </h2>    
-                        <form>
+                        <form
+                            ref={formRef}
+                            onSubmit={handleSubmit}>
                             <input
                                 className='file' 
                                 type="file" />
                             <input
+                                onChange={(e) => setNewName(e.target.value)}
                                 className='org' 
                                 type="text" 
                                 placeholder={lang === 'eng' ? recLang.eng.inputOrgPl: lang === 'rus' ? recLang.ru.inputOrgPl : recLang.am.inputOrgPl}/>
                             <input
+                                onChange={(e) => setNewRelation(e.target.value)}
                                 className='relation' 
                                 type="text" 
                                 placeholder={lang === 'eng' ? recLang.eng.relationship : lang === 'rus' ? recLang.ru.relationship : recLang.am.relationship}/>
                             <textarea
+                            onChange={(e) => setNewRecommend(e.target.value)}
                             placeholder={lang === 'eng' ? recLang.eng.textareaPl : lang === 'rus' ? recLang.ru.textareaPl: recLang.am.textareaPl} />
                             <button>
                                 <div 
--- src/store/slices/recommendSlices/recommendslices.js
+++ src/store/slices/recommendSlices/recommendslices.js
@@ -1,15 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { db } from "../../../FrebaseConfig/Config";
+import { collection, addDoc } from 'firebase/firestore'
 
 const recommendslices = createSlice({
     name: 'recommend',
     initialState: [],
     reducers: {
-        
+        addNewRec(store, {payload}) {
+            const usersCollectionRef = collection(db, "recommend")
+
+            const createUser = async () => {
+                await addDoc(usersCollectionRef, {name: payload.name, relation: payload.relation, recommend: payload.recommend })
+              }
+              createUser()
+        }
     }
 })
 
 export const selectrecommend = state => state.recommend
 
-export const {} = recommendslices.actions
+export const {addNewRec} = recommendslices.actions
 
 export const recommendReducer = recommendslices.reducer
